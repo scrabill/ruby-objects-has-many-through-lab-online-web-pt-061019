510e3d6ce2aa3c46b2898c70c7ab990493f5fb7f
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-21 19:06:03 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,38 @@
+require "pry"
+
+class Genre
+
+  attr_accessor :name, :songs, :artists
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+
+  def songs
+    Songs.all
+
+    #
+    # @songs
+    binding.pry
+  end
+
+  def artists
+    self.all.artists.name
+    # binding.pry
+  end
+end
+
+
+# The Genre class needs a class variable @@all that begins as an empty array.
+# The Genre class needs a class method .all that lists each genre in the class variable.
+# A genre should be initialized with a name and be saved in the @@all array.
+# The Genre class needs an instance method, #songs, that iterates through all songs and finds the songs that belong to that genre.
+# The Genre class needs an instance method, #artists, that iterates over the genre's collection of songs and collects the artist that owns each song.
